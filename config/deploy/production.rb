06cba743824f0905c656f67512d51a9ab65e259f
jesse.zha <jesse.zha@swwcc.com.au>
2018-06-19 11:54:33 +1000

    changed server ip address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,6 @@ set :rails_env,   "production"
 # This is the public IP, if we deploy through VPN we will have to change this
 # set :servers,     ["202.8.8.238"]
 # These are the internal IP addresses if you are connected via VPN
-set :servers,     ["119.225.67.213"]
+set :servers,     ["47.74.70.59"]
 # Changed to master as we are deploying master to the staging server for pre-live testing
 set :branch,      "master"
